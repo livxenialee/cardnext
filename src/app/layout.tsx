96b4f6cb372f7f3a0ed6af6b590db6c3d3ac2b0a
livxenialee <livxenialee@gmail.com>
2025-01-08 11:27:05 +0100

    Done?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
 import MyNav from "@/components/MyNav";
 import Footer from "@/components/Footer";
 
@@ -28,7 +29,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <MyNav />
-        {children}
+        <Suspense>{children}</Suspense>
         <Footer />
       </body>
     </html>
